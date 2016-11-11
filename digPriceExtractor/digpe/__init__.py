d6335c0baa56ade0ffdf907fc61ef9d0d1d07bf3
ZwEin <zwein27@gmail.com>
2016-11-11 15:02:25 -0800

    update price extract logic

--- digPriceExtractor/digpe/__init__.py
+++ digPriceExtractor/digpe/__init__.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-06-30 11:29:35
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-02 15:12:34
+# @Last Modified time: 2016-11-11 15:01:38
 
 from preprocessor import ZEPreprocessor
 from extractor import ZEExtractor
@@ -34,22 +34,43 @@ class DIGPriceExtractor():
         normalized_text_list = self.normalizer.normalize_from_list(extracted_text_list)
         ans = {}
         ans.setdefault(PE_DICT_NAME_PRICE, [])
-        ans.setdefault(PE_DICT_NAME_PPH, [])
+        # ans.setdefault(PE_DICT_NAME_PPH, [])
+
+        hour_data = []
+        # import json
+        # print json.dumps(cleaned_text_list, indent=4)
         for normalized in normalized_text_list:
             if not normalized[PE_JSON_NAME_TIME_UNIT]:
-                ans[PE_DICT_NAME_PPH].append(normalized[PE_JSON_NAME_PRICE])
+                # ans[PE_DICT_NAME_PPH].append(normalized[PE_JSON_NAME_PRICE])
+                hour_data.append(normalized)
             else:
                 tunit = DIGPriceExtractor.re_alphabet.findall(normalized[PE_JSON_NAME_TIME_UNIT])
                 if tunit and tunit[0].strip() in UNIT_TIME_HOUR:
-                    if tunit[0].strip() in UNIT_TIME_HOUR:
-                        digits = DIGPriceExtractor.re_digits.findall(normalized[PE_JSON_NAME_TIME_UNIT])
-                        if not digits or int(digits[0]) == 1:
-                            # ans.append(normalized)
-                            ans[PE_DICT_NAME_PPH].append(normalized[PE_JSON_NAME_PRICE])
+                    hour_data.append(normalized)
+                    # digits = DIGPriceExtractor.re_digits.findall(normalized[PE_JSON_NAME_TIME_UNIT])
+                    # if not digits or int(digits[0]) == 1:
+                        # ans[PE_DICT_NAME_PPH].append(normalized[PE_JSON_NAME_PRICE])
 
             ans[PE_DICT_NAME_PRICE].append(normalized)
-        return ans
 
+        # load price per hour data
+        price_per_hour = []
+        for hd in hour_data:
+            digits = DIGPriceExtractor.re_digits.findall(hd[PE_JSON_NAME_TIME_UNIT])
+            if not digits or int(digits[0]) == 1:
+                price_per_hour.append(int(hd[PE_JSON_NAME_PRICE]))
+        ans[PE_DICT_NAME_PPH] = 1*sum(price_per_hour)/len(price_per_hour) if len(price_per_hour) else 0
+
+        return ans
 
     def extract_from_list(self, text_list):
         return [self.extract(text) for text in text_list]
+
+
+if __name__ == '__main__':
+    text = "\n \n \n \n \n \n Escort's Phone: \n \n \n786-506-4729  \n \n Escort's Location: \nMiami, Florida  \n Escort's Age:   21   Date of Escort Post:   Oct 26th 9:01am \n REVIEWS:   \n REVIEWS FOUND!!! READ BEFORE YOU CALL   \n \n \n \n \n \nThere are  49  girls looking in  Buffalo .\n VIEW GIRLS \n \nDime Piece My Name Is Dime I'll assure You With Me You'll Have An Unforgettable Time No need To Hide Let The Freak In you Shine Come On This Ride NO EXCHANGE OF PICS Feel Free to FaceTime 7865064729 Donations $150/30m  $200 full hour (Unrushed) 420 INCALL & OUTCALLS-TODAY 7865064729. Call  786-506-4729 . I offer video/photo also, ask me on my profile before calling...  EasySex \n   \n \n   \n \n   \n Call me on my cell at 786-506-4729. Date of ad: 2014-10-26 09:01:00 \n More posts from  786-506-4729 \n 786-506-4729   Dec 05, 2014  UNFORGETTABLE petite FLEXiABLE Latina R2P - 21 \n \n \n 786-506-4729   Dec 03, 2014  100QVSLiM SEXi SLiNDERBack In Your Area - 21 \n \n \n 786-506-4729   Nov 29, 2014  NEW KiTYY ALERT R2P 18yrs Young - 18 \n \n \n 786-506-4729   Nov 15, 2014  100qsSiZZiLiNG HOT LATiNAFantasyGirl - 21 \n \n \n 786-506-4729   Nov 03, 2014  iM BACK Small Sexy Petite100Qs NewPics \n \n \n 786-506-4729   Oct 10, 2014  NEW GIRLS HAVE 1or BOTH The Choice Is Urs - 19 \n \n \n 786-506-4729   Sep 18, 2014  PuertoRican Barbie Small Slender SexyCALL ME IM WAITING... - 21 \n \n \n 786-506-4729   Sep 17, 2014  IM BACKKK Playmate Latina4'9 Bubble Butt R2P - 21 \n \n \n 786-506-4729   Sep 11, 2014  PYT FROM MIAMI LIMITED TIME ONLY. 4'9 petite Latina - 21 \n \n \n 786-506-4729   Aug 07, 2014  Sexay Mami - 21 \n \n \n 786-506-4729   Jun 29, 2014  LATINA FROM MIAMI IN TOWN 1DAY catch me if you can. NewPICS&VIDim waiting. - 21 \n \n \n 786-506-4729   Aug 21, 2013  SEXY LATINA no fake pics %100 real in/outcall - 20 \n \n \n 786-506-4729   Jul 31, 2013  Young & Sexy Latina - 20 \n \n \n 786-506-4729   Jul 20, 2013  Dime Piece %100 Real Pics In/Outcall $150 hh $200 h - 21 \n \n \n 786-506-4729   Jul 04, 2013  Dime Piece %100 Real Pics In/Outcall $150 hh $200 h - 21 - 21 \n \n \n 786-506-4729   Jun 05, 2013  Sexy Young Latina from Miami New in Town % 100 Real Pics in/out call - 21 \n \n \n 786-506-4729   May 09, 2013  Dime Escorts New Girls %100 Real Pics In/Outcall $200 - 21 \n \n \n 786-506-4729   Apr 09, 2013  2 Girl Special April & Dime %100 Real Pics In/Outcall - 20 \n \n \n \n \n \n \n"
+
+    dp = DIGPriceExtractor()
+    ans = dp.extract(text)
+    import json
+    print json.dumps(ans, indent=4)
--- digPriceExtractor/digpe/extractor.py
+++ digPriceExtractor/digpe/extractor.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-07-01 13:17:49
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-02 15:10:53
+# @Last Modified time: 2016-11-11 15:01:44
 
 import re
 
@@ -45,8 +45,6 @@ class ZEExtractor():
     # r'(?:' + reg_separator + r'for' + reg_separator + r')?' + \
 
 
-    
-
     ## pattern for time #
     reg_time_price = r'(?:' + \
             reg_time_units + \
@@ -75,6 +73,10 @@ class ZEExtractor():
     def filter(self, text_list):
         ans = []
         for text in text_list:
+            if '$' in text:
+                ans.append(text)
+                continue
+
             if ZEExtractor.re_alphabet.findall(text):
                 digits = ZEExtractor.re_digits.findall(text)
                 if len(digits) == 1 and int(digits[0]) < 5:
@@ -82,21 +84,43 @@ class ZEExtractor():
                 ans.append(text)
         return ans
 
+    def load_most_potential_target(self, pool):
+        count_length_array = [sum([len(item) for item in _ if not item.strip().isdigit()]) for _ in pool]
+        count_token_array = [len([item for item in _ if not item.strip().isdigit()]) for _ in pool]
+        count_dollar_array = [len([item for item in _ if item if '$' in item]) for _ in pool]
+
+        scores = [0]*len(pool)
+        for _ in [count_length_array, count_token_array, count_dollar_array]:
+            for i in range(len(scores)):
+                scores[i] += _[i]
+
+        # print '\n\n\n'
+        # print scores
+        # print 'count_length_array', count_length_array
+        # print 'count_token_array', count_token_array
+        # print 'count_dollar_array', count_dollar_array
+     
+        return pool[scores.index(max(scores))]
 
     def extract(self, text):
         text_pt_ext = ZEExtractor.re_price_time.findall(text)
         text_tp_ext = ZEExtractor.re_time_price.findall(text)
         text_op_ext = ZEExtractor.re_only_price.findall(text)
-     
+
+        # print text
+        # print text_pt_ext
+        # print text_tp_ext
+        # print text_op_ext
+
         if len(text_pt_ext) > len(text_tp_ext):
             target = text_pt_ext
         elif len(text_pt_ext) < len(text_tp_ext):
             target = text_tp_ext
         else:
             pool = [text_tp_ext, text_pt_ext, text_op_ext]
-            pool_max_len = [sum([len(item) for item in _]) for _ in pool]
-            target = pool[pool_max_len.index(max(pool_max_len))]
-
+            target = self.load_most_potential_target(pool)
+            # print pool, target 
+        # print target
         extra = []
         target_digits = ZEExtractor.re_digits.findall(' '.join(target))
 
@@ -108,7 +132,6 @@ class ZEExtractor():
                     extra.append(op_ext)
                     break
         target += extra
-
         return self.filter(target)
 
     def extract_from_list(self, text_list):
